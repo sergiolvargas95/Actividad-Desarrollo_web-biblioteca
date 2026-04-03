c0697672878d4ed84ddda46c7b0d4cf2762d1161
Sergio Vargas <sergiolvargas95@gmail.com>
2026-04-03 13:08:59 -0500

    feat(application) - add new commands dtos for users

---
+++ Application/Services/Dto/Commands/CreateUserCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+final class CreateUserCommand
+{
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $role;
+
+    public function __construct(
+        string $id,
+        string $name,
+        string $email,
+        string $password,
+        string $role
+    )
+    {
+        $this->id       = trim($id);
+        $this->name     = trim($name);
+        $this->email    = trim($email);
+        $this->password = trim($password);
+        $this->role     = trim($role);
+    }
+
+    public function getId(): string {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function getRole(): string {
+        return $this->role;
+    }
+}
